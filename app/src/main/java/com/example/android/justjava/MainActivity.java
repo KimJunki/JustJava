2f39ca393e52f47176da776d882d299d4a69f6a7
KimJunki <mkc0708@naver.com>
2016-07-18 16:17:59 +0800

    lessons3

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.android.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -21,11 +27,19 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void increment(View view) {
+        if (numberOfCoffees == 100) {
+            Toast.makeText(this, "you cannot have more than 100 coffees", Toast.LENGTH_SHORT).show();
+            return;
+        }
         numberOfCoffees= numberOfCoffees + 1;
         display(numberOfCoffees);
     }
 
     public void decrement(View view) {
+        if (numberOfCoffees == 1) {
+            Toast.makeText(this, "you cannot have less than 1 coffees", Toast.LENGTH_SHORT).show();
+            return;
+        }
         numberOfCoffees = numberOfCoffees - 1;
         display(numberOfCoffees);
     }
@@ -34,10 +48,55 @@ public class MainActivity extends ActionBarActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = numberOfCoffees * 5;
-        String priceMessage = "total $" + price;
-        priceMessage = priceMessage + "\nthank you!";
-        displayMessage(priceMessage);
+        EditText text = (EditText)findViewById(R.id.name_field);
+        String name = text.getText().toString();
+
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolateCheckBox.isChecked();
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
+        String priceMessage = createOrderSummary(name, price, hasWhippedCream, hasChocolate);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java order for " + name);
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    /**
+     * Calculates the price of the order.
+     */
+    private int calculatePrice(boolean addWhippedCream, boolean addChocolate) {
+
+        int basePrice = 5;
+
+        if (addWhippedCream) {
+            basePrice = basePrice + 1;
+        }
+
+        if (addChocolate) {
+            basePrice = basePrice + 2;
+        }
+        int price = numberOfCoffees * basePrice;
+        return price;
+    }
+
+    /**
+     * This method is create ordersummary.
+     */
+    private String createOrderSummary(String name, int price, boolean addWhippedCream, boolean addChocolate) {
+        String priceMessage = "Name : " + name;
+        priceMessage += "Add whipped cream? " + addWhippedCream;
+        priceMessage += "\nAdd chocolate? " + addChocolate;
+        priceMessage += "\nQuantity : " + numberOfCoffees;
+        priceMessage += "\nTotal : $" + price;
+        priceMessage += "\nThank you!";
+        return priceMessage;
     }
 
     /**
@@ -52,16 +111,4 @@ public class MainActivity extends ActionBarActivity {
     /**
      * This method displays the given price on the screen.
      */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
-
-    /**
-     * This method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
-    }
 }
